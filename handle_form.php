f223521776912b308591c64b8fe621c1e3020467
Nissaq <2020470222@student.uitm.edu.my>
2022-05-07 21:02:41 +0800

    handle_form.php

--- handle_form.php
+++ handle_form.php
@@ -8,9 +8,9 @@
 </head>
 <body>
     <?php #Script handle form
-    echo "<p> Thank you, <b>".$_POST["name"].
-    "</b> for the following comments : <br/>".
-    $_POST["comments"]."<p>We will reply to you at <i>".
+    echo "<p> Thank you, <b>".$_POST["name"];
+    "</b> for the following comments : <br/>";
+    $_POST["comments"]."<p>We will reply to you at <i>";
     $_POST["email"]."</i></p>";
     ?>
 </body>
